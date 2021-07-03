f2238d05c5011adffef3dc8124460f2cd902bac7
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2021-07-03 11:37:19 -0300

    Remove sslr-benchmarks module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ rootProject.name = "sslr"
 include(":sslr-testing-harness")
 include(":sslr-examples")
 include(":sslr-core")
-include(":sslr-benchmarks")
 include(":sslr-xpath")
 include(":sslr-toolkit")
 include(":sslr-tests")
--- sslr-benchmarks/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    implementation("org.openjdk.jmh:jmh-core:1.15")
-    implementation(project(":sslr-core"))
-    testImplementation("org.easytesting:fest-assert:1.4")
-}
-
-description = "SonarSource :: Language Recognizer :: Benchmarks"
