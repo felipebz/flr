242e4642af941d67ad6534e2398574220db52e3b
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2021-07-09 20:33:14 -0300

    Update Jaxen version

--- flr-xpath/build.gradle.kts
+++ flr-xpath/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     implementation(project(":flr-core"))
-    implementation("jaxen:jaxen:1.1.4")
+    implementation("jaxen:jaxen:1.2.0")
     testImplementation("org.easytesting:fest-assert:1.4")
     testImplementation("org.mockito:mockito-core:3.11.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
