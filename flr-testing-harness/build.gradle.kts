37acd5cda3cfb8eae92bb1c42605a9b021d64a37
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2021-09-04 16:35:31 -0300

    Declare some dependencies as "api" in flr-testing-harness

--- flr-testing-harness/build.gradle.kts
+++ flr-testing-harness/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
     implementation(project(":flr-core"))
     implementation(project(":flr-toolkit"))
-    implementation("org.easytesting:fest-assert:1.4")
-    implementation("org.hamcrest:hamcrest:2.2")
+    api("org.easytesting:fest-assert:1.4")
+    api("org.hamcrest:hamcrest:2.2")
     implementation(platform("org.junit:junit-bom:5.7.2"))
     implementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.hamcrest:hamcrest-library:2.2")
