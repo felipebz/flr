145bfbc84b82e71ef12e056aa0f7b015f8481e10
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2023-01-02 20:43:59 -0300

    build: Update copyright in license headers

--- flr-core/src/main/kotlin/com/felipebz/flr/impl/LexerOutput.kt
+++ flr-core/src/main/kotlin/com/felipebz/flr/impl/LexerOutput.kt
@@ -1,6 +1,6 @@
 /**
  * FLR
- * Copyright (C) 2021-2021 Felipe Zorzo
+ * Copyright (C) 2021-2023 Felipe Zorzo
  * mailto:felipe AT felipezorzo DOT com DOT br
  *
  * This program is free software; you can redistribute it and/or
--- flr-core/src/test/kotlin/com/felipebz/flr/internal/vm/NothingExpressionTest.kt
+++ flr-core/src/test/kotlin/com/felipebz/flr/internal/vm/NothingExpressionTest.kt
@@ -1,7 +1,7 @@
 /**
  * FLR
- * Copyright (C) 2010-2021 SonarSource SA
- * Copyright (C) 2021-2021 Felipe Zorzo
+ * Copyright (C) 2010-2023 SonarSource SA
+ * Copyright (C) 2021-2023 Felipe Zorzo
  * mailto:felipe AT felipezorzo DOT com DOT br
  *
  * This program is free software; you can redistribute it and/or
